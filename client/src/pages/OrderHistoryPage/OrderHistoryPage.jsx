0a1866616d275b07654ae4d964a20ba65ea56812
Platin Syla <platinsyla06@gmail.com>
2023-07-13 21:14:36 -0400

    Added some basic style to the orderhistorypage

--- client/src/pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ client/src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { getOrderHistory, getOrderHistoryById } from "../../services/OrderServices";
 import { useNavigate } from "react-router-dom";
-
+import "./OrderHistoryPage.css";
 
 const OrderHistoryPage = ({ user }) => {
     const navigate = useNavigate();
